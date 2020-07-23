e8b0a7c01bba712aa88154954e5c83b521e98aaf
kotiakhova <kotiakhovaa@gmail.com>
2020-07-23 16:28:32 +0300

    Updates

---
+++ precache-manifest.35f1e37cefb78ca3961ff0ff90a99a44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb6174be62c133d3956675d12c1c0ec",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "5c1fbb12f1c66ca8ad97",
+    "url": "/goit-react-hw-01-components/static/css/main.7d86e790.chunk.css"
+  },
+  {
+    "revision": "4660c619a268afbe82d3",
+    "url": "/goit-react-hw-01-components/static/js/2.204ef07a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.204ef07a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c1fbb12f1c66ca8ad97",
+    "url": "/goit-react-hw-01-components/static/js/main.563e320c.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
